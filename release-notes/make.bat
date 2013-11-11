3ad0a6867412c8ee67fe999330d44953db11218d
Stephan Linz <stephan.linz@clondiag.com>
2013-11-11 13:10:33 +0100

    content: fix some naming mismatches
    Signed-off-by: Stephan Linz <stephan.linz@clondiag.com>

--- release-notes/make.bat
+++ release-notes/make.bat
@@ -99,9 +99,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\AlereCommunityBSPReleaseNotes.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\TQSystemsCommunityBSPReleaseNotes.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\AlereCommunityBSPReleaseNotes.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\TQSystemsCommunityBSPReleaseNotes.ghc
 	goto end
 )
 
